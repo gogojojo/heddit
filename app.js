eed1146da0b09f5c23820c7aaf4a811657baa2f6
gogojojo <joseph.gochua@gmail.com>
2015-03-30 11:32:06 -0700

    added routing

--- app.js
+++ app.js
@@ -1,10 +1,32 @@
-angular.module('poster', [])
+angular.module('poster', ['ui.router'])
+
+.config([
+'$stateProvider',
+'$urlRouterProvider',
+function($stateProvider, $urlRouterProvider) {
+
+  $stateProvider
+    .state('home', {
+      url: '/home',
+      templateUrl: '/home.html',
+      controller: 'postercontroller'
+    });
+    .state('posts', {
+      url: '/posts/{id}',
+      templateUrl: '/posts.html',
+      controller: 'PostsCtrl'
+    });
+
+  $urlRouterProvider.otherwise('home');
+}])
+
 .factory('posts', [function(){
    var o = {
     posts: []
   };
   return o;
 }])
+
 .controller('postercontroller', [
 '$scope',
 'posts',
