f70efe0972abdbd0da9dc629e47dc201799f89e1
gogojojo <joseph.gochua@gmail.com>
2015-03-30 11:14:22 -0700

    adds link to sumbit

--- app.js
+++ app.js
@@ -13,10 +13,11 @@ function($scope){
   ];
   $scope.addPost = function(){
   if(!$scope.title || $scope.title === '') { return; }
-  $scope.posts.push({title: $scope.title, upvotes: 0});
-  $scope.title = ''
-  };
-  $scope.incrementUpvotes = function(post) {
-    post.upvotes += 1;
-  };
+  $scope.posts.push({
+    title: $scope.title,
+    link: $scope.link,
+    upvotes: 0
+  });
+  $scope.title = '';
+  $scope.link = '';
 }]);
